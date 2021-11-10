51d9c7c926fd992ff3032975673360acd2b847f2
jdenazareth <nazarethyleony@gmail.com>
2021-11-09 21:15:26 -0600

    sale Order
    Se quito campos inesesarios

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -8,8 +8,8 @@ class SaleOrderLine(models.Model):
     ji_area = fields.Float(string="Area", compute="_compute_ji_product_information", store=True)
     ji_street = fields.Char(string="Street", compute="_compute_ji_product_information", store=True)
     ji_corner_with = fields.Char(string="Corner With", compute="_compute_ji_product_information", store=True)
-    ji_manzana = fields.Char(string="Manzana", compute="_compute_ji_product_information", store=True)
-    ji_lote = fields.Char(string="Lote", compute="_compute_ji_product_information", store=True)
+    #ji_manzana = fields.Char(string="Manzana", compute="_compute_ji_product_information", store=True)
+    #ji_lote = fields.Char(string="Lote", compute="_compute_ji_product_information", store=True)
 
     @api.depends("product_id")
     def _compute_ji_product_information(self):
@@ -17,5 +17,5 @@ class SaleOrderLine(models.Model):
             line.ji_area = line.product_id.ji_area
             line.ji_street = line.product_id.ji_street
             line.ji_corner_with = line.product_id.ji_corner_with
-            line.ji_manzana = line.product_id.x_studio_manzana.name
-            line.ji_lote = line.product_id.x_studio_lote.name
+           # line.ji_manzana = line.product_id.x_studio_manzana.name
+           # line.ji_lote = line.product_id.x_studio_lote.name
