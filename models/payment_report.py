50371de25f857b085d6e72ccd4621f002e9db749
jdenazareth <nazarethyleony@gmail.com>
2021-12-14 17:29:52 -0600

    f

--- models/payment_report.py
+++ models/payment_report.py
@@ -14,12 +14,14 @@ class ReportePayment(models.Model):
     pago_mensualidad_letra = fields.Char(string="pagos en letra", compute="get_text_amount_mensaulidad")
     residual = fields.Float(string="Residual ", compute="get_origin")
     residual_letra =  fields.Char(strin="Residual letra", compute="get_text_amount_mensaulidad")
+    total_factura = fields.Float(string="Total Facturas", compute="get_origin")
     def get_text_amount_total(self):
         self.cantidad_letra = num2words(self.amount, lang='es').upper()
 
     def get_origin(self):
         datos=self.env['account.move'].search([('name','=',self.communication)]) #consulta
         self.dia_dato_pago = datos.invoice_payment_term_id.dia_dato
+        self.total_factura = datos.amount_total
         self.residual = datos.amount_residual
         self.pagos_mensualidad = datos.amount_total - datos.porcentaje
         self.pagos_mensualidad =  self.pagos_mensualidad / datos.invoice_payment_term_id.ji_numbers_monthly
